9c6b6a7945abf0297b006a1d578d9ae76b91d4b7
Niranjan Jayakar <nija@amazon.com>
2019-11-19 11:05:05 +0000

    Dotnet serverless

---
+++ dotnet/serverless/src/Serverless/Program.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serverless
+{
+    public class MyAppFunction : FunctionBase {
+        MyAppFunction(Construct scope, string id) : base(scope, id) {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App(null);
+            var stack = new Stack(app, "mystack");
+            var fn = new Function(stack, "myfunction", new FunctionProps {
+                Code = Code.FromInline("my code here"),
+                Handler = "index.handler",
+                Runtime = Runtime.NODEJS_8_10
+            });
+            CfnFunction cfnfn = (CfnFunction) fn.Node.DefaultChild;
+            cfnfn.AddPropertyOverride("Runtime", "nodejs10.x");
+
+            var version = new CfnVersion(stack, $"{fn.Node.Id}-Version", new CfnVersionProps {
+                FunctionName = fn.FunctionName
+            });
+            version.Node.AddDependency(cfnfn);
+
+            var myappfn = new MyAppFunction(stack, "myappfunction");
+            // Console.WriteLine(myappfn.FunctionName);
+
+            app.Synth();
+        }
+    }
+    }
+}
