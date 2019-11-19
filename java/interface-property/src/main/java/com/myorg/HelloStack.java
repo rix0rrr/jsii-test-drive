9cefddb2b3bfb0b3aa7184bd41c4355c062ca55d
Adam Ruka <adamruka@amazon.com>
2019-11-19 14:24:30 -0800

    Rename Java's 'callback' project to 'interface-property'.

--- java/interface-property/src/main/java/com/myorg/HelloStack.java
+++ java/interface-property/src/main/java/com/myorg/HelloStack.java
@@ -10,10 +10,11 @@ import software.amazon.awscdk.services.codepipeline.Pipeline;
 import software.amazon.awscdk.services.codepipeline.StageProps;
 import software.amazon.awscdk.services.codepipeline.actions.CodeBuildAction;
 import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
-import software.amazon.awscdk.services.ecs.Compatibility;
-import software.amazon.awscdk.services.ecs.ContainerDefinition;
-import software.amazon.awscdk.services.ecs.LinuxParameters;
-import software.amazon.awscdk.services.ecs.TaskDefinition;
+import software.amazon.awscdk.services.events.EventPattern;
+import software.amazon.awscdk.services.events.IRule;
+import software.amazon.awscdk.services.events.IRuleTarget;
+import software.amazon.awscdk.services.events.Rule;
+import software.amazon.awscdk.services.events.RuleTargetConfig;
 
 import static java.util.Arrays.asList;
 
@@ -24,37 +25,6 @@ public class HelloStack extends Stack {
 
     public HelloStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
-
-        // tests a callback getting a parameter be null
-        ContainerDefinition.Builder.create(this, "ContainerDef1")
-                .taskDefinition(
-                        TaskDefinition.Builder.create(this, "TaskDef1")
-                                .compatibility(Compatibility.EC2_AND_FARGATE)
-                                .memoryMiB("1024MiB")
-                                .cpu("24")
-                                .build()
-                )
-                .image(new MyContainerImageNoLinuxParams())
-                .memoryLimitMiB(1024)
-                .build();
-
-        // tests a callback getting a parameter being non-null
-        ContainerDefinition.Builder.create(this, "ContainerDef2")
-                .taskDefinition(
-                        TaskDefinition.Builder.create(this, "TaskDef2")
-                                .compatibility(Compatibility.EC2_AND_FARGATE)
-                                .memoryMiB("1024MiB")
-                                .cpu("24")
-                                .build()
-                )
-                .image(new MyContainerImageLinuxParams())
-                .linuxParameters(
-                        LinuxParameters.Builder.create(this, "LinuxParameters")
-                            .initProcessEnabled(true)
-                            .build()
-                )
-                .memoryLimitMiB(1024)
-                .build();
 
         // this doesn't work (the custom CodePipeline action)
         Artifact sourceOutput = new Artifact();
