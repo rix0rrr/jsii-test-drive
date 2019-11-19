c1e6fbf986e3a64266bbba634fe82059982920fe
Niranjan Jayakar <nija@amazon.com>
2019-11-19 15:18:27 +0000

    Adding an aspect

--- dotnet/serverless/src/Serverless/Program.cs
+++ dotnet/serverless/src/Serverless/Program.cs
@@ -8,9 +8,22 @@ namespace Serverless
 {
     public class MyAppFunction : FunctionBase
     {
-        public MyAppFunction(Construct scope, string id) : base(scope, id) {
+        public MyAppFunction(Construct scope, string id) : base(scope, id)
+        {
         }
     }
+
+    public class Node12Aspect : IAspect
+    {
+        public void Visit(IConstruct construct)
+        {
+            if (construct.GetType().Name.CompareTo("CfnFunction") == 0)
+            {
+                ((CfnResource) construct).AddPropertyOverride("Runtime", "nodejs12.x");
+            }
+        }
+    }
+
 
     class Program
     {
@@ -33,6 +46,8 @@ namespace Serverless
 
             var myappfn = new MyAppFunction(stack, "myappfunction");
             // Console.WriteLine(myappfn.FunctionName);
+
+            stack.Node.ApplyAspect(new Node12Aspect());
 
             app.Synth();
         }
